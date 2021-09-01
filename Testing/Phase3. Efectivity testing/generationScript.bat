96240f8567c0f00f23cb4fe23ef8d416e51744c3
pepedg <josediaz.swengineering@gmail.com>
2021-09-01 12:37:27 +0200

    v1.0

---
+++ Testing/Phase3. Efectivity testing/generationScript.bat
@@ -0,0 +1,110 @@
+ECHO getting proccessed models...
+
+cd ..
+cd ..
+
+REM RArm Random
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant1.use" RArm Random -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant1_generated.use" "Testing\Phase3. Efectivity testing\RArm\Random\Mutant1_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant2.use" RArm Random -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant2_generated.use" "Testing\Phase3. Efectivity testing\RArm\Random\Mutant2_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant3.use" RArm Random -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant3_generated.use" "Testing\Phase3. Efectivity testing\RArm\Random\Mutant3_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant4.use" RArm Random -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant4_generated.use" "Testing\Phase3. Efectivity testing\RArm\Random\Mutant4_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant5.use" RArm Random -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant5_generated.use" "Testing\Phase3. Efectivity testing\RArm\Random\Mutant5_generated.use"
+
+
+REM RArm Depth
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant1.use" RArm Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant1_generated.use" "Testing\Phase3. Efectivity testing\RArm\Depth\Mutant1_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant2.use" RArm Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant2_generated.use" "Testing\Phase3. Efectivity testing\RArm\Depth\Mutant2_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant3.use" RArm Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant3_generated.use" "Testing\Phase3. Efectivity testing\RArm\Depth\Mutant3_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant4.use" RArm Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant4_generated.use" "Testing\Phase3. Efectivity testing\RArm\Depth\Mutant4_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant5.use" RArm Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant5_generated.use" "Testing\Phase3. Efectivity testing\RArm\Depth\Mutant5_generated.use"
+
+
+REM RArm Breadth
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant1.use" RArm Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant1_generated.use" "Testing\Phase3. Efectivity testing\RArm\Breadth\Mutant1_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant2.use" RArm Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant2_generated.use" "Testing\Phase3. Efectivity testing\RArm\Breadth\Mutant2_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant3.use" RArm Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant3_generated.use" "Testing\Phase3. Efectivity testing\RArm\Breadth\Mutant3_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant4.use" RArm Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant4_generated.use" "Testing\Phase3. Efectivity testing\RArm\Breadth\Mutant4_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\RArm\Mutant5.use" RArm Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\RArm\Mutant5_generated.use" "Testing\Phase3. Efectivity testing\RArm\Breadth\Mutant5_generated.use"
+
+
+
+REM Microwave Random
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant1.use" Microwave Random -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant1_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Random\Mutant1_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant2.use" Microwave Random -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant2_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Random\Mutant2_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant3.use" Microwave Random -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant3_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Random\Mutant3_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant4.use" Microwave Random -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant4_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Random\Mutant4_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant5.use" Microwave Random -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant5_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Random\Mutant5_generated.use"
+
+
+REM Microwave Depth
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant1.use" Microwave Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant1_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Depth\Mutant1_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant2.use" Microwave Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant2_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Depth\Mutant2_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant3.use" Microwave Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant3_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Depth\Mutant3_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant4.use" Microwave Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant4_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Depth\Mutant4_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant5.use" Microwave Depth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant5_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Depth\Mutant5_generated.use"
+
+
+REM Microwave Breadth
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant1.use" Microwave Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant1_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Breadth\Mutant1_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant2.use" Microwave Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant2_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Breadth\Mutant2_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant3.use" Microwave Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant3_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Breadth\Mutant3_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant4.use" Microwave Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant4_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Breadth\Mutant4_generated.use"
+
+java -jar USETestGenerator.jar "Testing\Phase3. Efectivity testing\Microwave\Mutant5.use" Microwave Breadth -10 10
+move /y "Testing\Phase3. Efectivity testing\Microwave\Mutant5_generated.use" "Testing\Phase3. Efectivity testing\Microwave\Breadth\Mutant5_generated.use"
+
+
+ECHO done!
+PAUSE
